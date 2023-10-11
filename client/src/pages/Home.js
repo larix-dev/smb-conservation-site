cb93b56f4c9c4368ea9775442b103caace9bb66c
John Yorke <yorkejohn02@gmail.com>
2023-10-11 14:11:27 -0300

    (SMU-3) Fix #Fix opacity

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,7 +14,7 @@ function Home() {
         <br />
         Nature and Heritage
       </div>
-      <Link to="about" className="accent-button opacity-90">
+      <Link to="about" className="accent-button">
         Learn More&nbsp;
         <HiSparkles className="inline" />
       </Link>
