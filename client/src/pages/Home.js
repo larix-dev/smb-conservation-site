b17458a9bc377a0ee108f4c49d72933516aaf99e
John Yorke <yorkejohn02@gmail.com>
2023-10-11 00:24:11 -0300

    (SMU-3) Feat #Add padding

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,7 +9,7 @@ function Home() {
       className="w-full bg-cover bg-center flex flex-col justify-center items-center gap-4"
       style={{backgroundImage: `url(${background})`}}
     >
-      <div className="text-4xl text-white font-black text-center">
+      <div className="text-4xl text-white font-black text-center px-8">
         Preserving Nova&nbsp;Scotia's
         <br />
         Nature and Heritage
