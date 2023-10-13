3e02ae1ca6465c0b3210d8208ddff2cf400245f4
John Yorke <yorkejohn02@gmail.com>
2023-10-13 16:11:27 -0300

    (SMU-3) Fix #Naming convention in Logo

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -6,7 +6,7 @@ function Logo() {
   return (
     <Link to="/">
       <div className="text-white flex gap-4 items-center font-logo font-semibold tracking-tighter">
-        <img src={logo_image} alt="Forest Logo" className="h-12" />
+        <img src={logoImage} alt="Forest Logo" className="h-12" />
         <span className="leading-tight">
           St. Margaret's Bay Area
           <br />
