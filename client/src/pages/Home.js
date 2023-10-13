f379af9170b0bae06074b0e7261b0206545a1320
John Yorke <yorkejohn02@gmail.com>
2023-10-13 16:13:14 -0300

    (SMU-3) Fix #Button class name

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,7 +14,7 @@ function Home() {
         <br />
         Nature and Heritage
       </div>
-      <Link to="about" className="accent-button">
+      <Link to="about" className="home-cta-button">
         Learn More&nbsp;
         <HiSparkles className="inline" />
       </Link>
