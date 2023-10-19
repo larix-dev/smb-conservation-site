84a7d9eaa0b175c0c7e71f9baf09621e46e63e33
ethan-cooke <ethandcooke@gmail.com>
2023-10-19 17:20:38 -0300

    (SMU-8) Feat #Added Mapbox

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,14 +1,43 @@
+import mapboxgl from 'mapbox-gl'
+import {useEffect, useRef, useState} from 'react'
+
 import Page from '../components/Page'
 import placeholderMap from '../assets/images/Temp_Map.jpg'
 
 function Map() {
-    return (
-        <Page name="Map">
-            <div class="flex justify-center items-center">
-                <img src={placeholderMap} alt="placeholder map"></img>
-            </div>
-        </Page>
-    )
+  return (
+    <Page name="Map">
+      <div>
+        <Mapbox />
+      </div>
+    </Page>
+  )
+}
+
+function Mapbox() {
+  mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
+
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng, setLng] = useState(-63.916881)
+  const [lat, setLat] = useState(44.624944)
+  const [zoom, setZoom] = useState(15)
+
+  useEffect(() => {
+    if (map.current) {
+      return
+    }
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/outdoors-v12',
+      center: [lng, lat],
+      zoom: zoom,
+      attributionControl: false
+    })
+    map.current.resize()
+  }, [])
+
+  return <div ref={mapContainer} className="w-full aspect-video"></div>
 }
 
 export default Map
