69177b1c9437d41c6993a170141cb6b5a78a0c26
John Yorke <yorkejohn02@gmail.com>
2023-10-20 16:01:05 -0300

    (SMU-15) feat #fix layout of the page

--- client/src/pages/Burial.js
+++ client/src/pages/Burial.js
@@ -19,7 +19,7 @@ function Burial() {
 
   const {loading, error, data} = useQuery(query)
 
-  if (loading || error) {
+  if (loading || error || !data) {
     return null
   }
 
@@ -27,19 +27,17 @@ function Burial() {
 
   return (
     <Page name="Green Burial: Resting in Nature's Embrace">
-      <div className="prose w-full">
-      <DocumentRenderer document={document} />
-      </div>
-      <div className="flex gap-8">
-        <div className="flex-1 prose">
-          <div className="inline">
-            <div className="flex-1">
-              <img src={burial} alt="Burial" />
-              <div>
-                <h1 className="text-2xl lg:text-4xl tracking-tight font-bold mb-4">Inquery form:</h1>
-                <Signup />
-              </div>
-            </div>
+      <div className="flex flex-col gap-8">
+        <div className="prose max-w-none">
+          <DocumentRenderer document={document} />
+        </div>
+        <div className="flex flex-col md:flex-row gap-8">
+          <div className="flex-1">
+            <img src={burial} alt="Burial" />
+          </div>
+          <div className="flex-1 bg-stone-300 p-4">
+            <div className="text-2xl lg:text-4xl tracking-tight font-bold mb-4">Get In Touch</div>
+            <Signup />
           </div>
         </div>
       </div>
