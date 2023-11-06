292a23a935792246aa5abaf0037bf1dadcaf071a
John Yorke <yorkejohn02@gmail.com>
2023-11-06 19:40:38 -0400

    (SMU-20) feat #add docker and db migrations
    * (SMU-20) feat #add deploy scripts
    
    * (SMU-20) feat #add ssh fingerprints, use DB_URL env var
    
    * (SMU-20) feat #add dockerfile, db migrations

--- scripts/deploy_client.sh
+++ scripts/deploy_client.sh
@@ -1,2 +1,3 @@
 ssh-keyscan $SERVER_HOSTNAME >> ~/.ssh/known_hosts
+sudo apt-get update && sudo apt-get install -y rsync
 rsync -a --delete ~/project/client/build/ $SERVER_USER@$SERVER_HOSTNAME:~/public_html/
