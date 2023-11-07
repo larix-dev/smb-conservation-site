15564e01ee90d99df653f37d7a2be1cb0e53e21c
ethan-cooke <ethandcooke@gmail.com>
2023-11-07 12:41:29 -0400

    (SMU-8) feat #static map url now relies on variables

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -5,9 +5,14 @@ import {Link} from 'react-router-dom'
 import Page from '../components/Page'
 
 function Map() {
+
+  const lon = -63.9196
+  const lat = 44.6247
+  const zoom = 14.92
+
   // this should be built based on the CMS. see https://docs.mapbox.com/api/maps/static-images/
   const mapSrc =
-    'https://api.mapbox.com/styles/v1/mapbox/outdoors-v12/static/-63.9196,44.6247,14.92,0/600x400@2x?access_token=pk.eyJ1IjoiZ3JvdXBmIiwiYSI6ImNsbnhsYWV1dDBoaXEybW8zMzRka3c0eWsifQ.cVCT0voobYso8dwg8t6ufQ'
+    `https://api.mapbox.com/styles/v1/mapbox/outdoors-v12/static/${lon},${lat},${zoom},0/600x400@2x?access_token=pk.eyJ1IjoiZ3JvdXBmIiwiYSI6ImNsbnhsYWV1dDBoaXEybW8zMzRka3c0eWsifQ.cVCT0voobYso8dwg8t6ufQ`
 
   return (
     <Page name="Conservation Site Map">
