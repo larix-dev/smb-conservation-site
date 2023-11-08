8726a5ec6fd9a09400b727b7bdf80405f6b6d284
John Yorke <yorkejohn02@gmail.com>
2023-11-07 21:29:45 -0400

    (SMU-20) fix #circleci config, deploy client in docker

--- scripts/deploy_client.sh
+++
@@ -1,3 +0,0 @@
-ssh-keyscan $SERVER_HOSTNAME >> ~/.ssh/known_hosts
-sudo apt-get update && sudo apt-get install -y rsync
-rsync -a --delete ~/project/client/build/ $SERVER_USER@$SERVER_HOSTNAME:~/public_html/
